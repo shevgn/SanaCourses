436babeb0b5de599d7f6f04eaffb9afedc026644
Artem Levchenko <evgn.main@gmail.com>
2023-10-24 19:10:06 +0300

    Program.cs new lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,4 @@
 // This is a comment\
-int main() \{\
-    printf(\'abHello world!\'bb);\
-    int a = 10;\
-    return 0;\
-\}}
+Console.WrileLine("Hello, World!");
+Console.WrileLine("Hello, World!");
+Console.ReadLine();
